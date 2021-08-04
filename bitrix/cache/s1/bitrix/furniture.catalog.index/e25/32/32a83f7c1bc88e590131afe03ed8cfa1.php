a7cebcd3fb3216156ccd8a719f8babab1ad05e9d
root <svsvpop-3d@mail.ru>
2021-08-04 17:25:09 +0300

    working build 1

---
+++ bitrix/cache/s1/bitrix/furniture.catalog.index/e25/32/32a83f7c1bc88e590131afe03ed8cfa1.php
@@ -0,0 +1,41 @@
+<?
+if($INCLUDE_FROM_CACHE!='Y')return false;
+$datecreate = '001628084853';
+$dateexpire = '001664084853';
+$ser_content = 'a:2:{s:7:"CONTENT";s:1441:"<div class="product-list">
+	<div class="product">
+		<div class="product-overlay"></div>
+		<div class="product-image" style="background-image: url(/upload/iblock/460/46020a80bb36e809c7f99c488e2fbc30.gif)"></div>
+		<a class="product-desc" href="/products/1/">
+			<b>Мягкая мебель</b>
+			<p>Диваны, кресла и прочая мягкая мебель</p>
+		</a>
+	</div>
+	<div class="product">
+		<div class="product-overlay"></div>
+		<div class="product-image" style="background-image: url(/upload/iblock/b69/b691352c14001ff648c9a70206ca2a85.gif)"></div>
+		<a class="product-desc" href="/products/2/">
+			<b>Офисная мебель</b>
+			<p>Диваны, столы, стулья</p>
+		</a>
+	</div>
+	<div class="product">
+		<div class="product-overlay"></div>
+		<div class="product-image" style="background-image: url(/upload/iblock/653/653f6a4957fddea033e98885337a168c.gif)"></div>
+		<a class="product-desc" href="/products/3/">
+			<b>Мебель для кухни</b>
+			<p>Полки, ящики, столы и стулья</p>
+		</a>
+	</div>
+	<div class="product">
+		<div class="product-overlay"></div>
+		<div class="product-image" style="background-image: url(/upload/iblock/824/82442a4aa8d45dc71ba5126d205c7fcb.gif)"></div>
+		<a class="product-desc" href="/products/4/">
+			<b>Детская мебель</b>
+			<p>Кровати, стулья, мягкая детская мебель</p>
+		</a>
+	</div>
+</div>
+";s:4:"VARS";a:2:{s:8:"arResult";a:1:{s:5:"ITEMS";a:4:{i:0;a:4:{s:4:"NAME";s:25:"Мягкая мебель";s:11:"DESCRIPTION";s:68:"Диваны, кресла и прочая мягкая мебель";s:10:"DETAIL_URL";s:12:"/products/1/";s:7:"PICTURE";a:3:{s:3:"SRC";s:55:"/upload/iblock/460/46020a80bb36e809c7f99c488e2fbc30.gif";s:5:"WIDTH";i:117;s:6:"HEIGHT";i:97;}}i:1;a:4:{s:4:"NAME";s:27:"Офисная мебель";s:11:"DESCRIPTION";s:38:"Диваны, столы, стулья";s:10:"DETAIL_URL";s:12:"/products/2/";s:7:"PICTURE";a:3:{s:3:"SRC";s:55:"/upload/iblock/b69/b691352c14001ff648c9a70206ca2a85.gif";s:5:"WIDTH";i:118;s:6:"HEIGHT";i:97;}}i:2;a:4:{s:4:"NAME";s:30:"Мебель для кухни";s:11:"DESCRIPTION";s:50:"Полки, ящики, столы и стулья";s:10:"DETAIL_URL";s:12:"/products/3/";s:7:"PICTURE";a:3:{s:3:"SRC";s:55:"/upload/iblock/653/653f6a4957fddea033e98885337a168c.gif";s:5:"WIDTH";i:115;s:6:"HEIGHT";i:97;}}i:3;a:4:{s:4:"NAME";s:27:"Детская мебель";s:11:"DESCRIPTION";s:70:"Кровати, стулья, мягкая детская мебель";s:10:"DETAIL_URL";s:12:"/products/4/";s:7:"PICTURE";a:3:{s:3:"SRC";s:55:"/upload/iblock/824/82442a4aa8d45dc71ba5126d205c7fcb.gif";s:5:"WIDTH";i:117;s:6:"HEIGHT";i:97;}}}}s:18:"templateCachedData";a:2:{s:9:"frameMode";N;s:12:"frameModeCtx";s:81:"/bitrix/components/bitrix/furniture.catalog.index/templates/.default/template.php";}}}';
+return true;
+?>
