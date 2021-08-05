2d3ea80133c26ca584b291f10e3c3ca0336e1139
user <svsvpop-3d@mail.ru>
2021-08-05 14:45:22 +0300

    2 done + fixed

---
+++ partners/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Условия сотрудничества", 
+		"/partners/index.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Расписание мероприятий", 
+		"/partners/events_calendar/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Как стать партнером", 
+		"/partners/how_to_become_a_partner.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
---
+++ partners/how_to_become_a_partner.php
@@ -0,0 +1,7 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Как стать партнером");
+$APPLICATION->SetPageProperty("description", "Порядок подачи заявки на партнерство и бланк партнерского
+договора");
+$APPLICATION->SetPageProperty("keywords", "заявка, партнерский договор");
+?>Text here....<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
