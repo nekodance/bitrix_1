b8d3e78029fdcd9023351aba6a60b178d4c0e866
user <svsvpop-3d@mail.ru>
2021-08-09 17:29:36 +0300

    4.2 almost done

--- test.php
+++ test.php
@@ -1,4 +1,15 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Тест");
-?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:system.auth.form", 
+	"auth_form", 
+	array(
+		"COMPONENT_TEMPLATE" => "auth_form",
+		"REGISTER_URL" => "/user/registration.php",
+		"FORGOT_PASSWORD_URL" => "/user/",
+		"PROFILE_URL" => "/user/profile.php",
+		"SHOW_ERRORS" => "Y"
+	),
+	false
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
